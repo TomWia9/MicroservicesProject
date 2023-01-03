4b48b214b56aa9ba96abd0c2abd496077f7603d2
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-01-03 08:10:26 +0100

    Fix serilog in CommandsService

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,11 +1,16 @@
 using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
     .WriteTo.Console()
     .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -13,10 +18,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Logging.ClearProviders();
-
-builder.Logging.AddSerilog();
 
 var app = builder.Build();
 
@@ -27,10 +28,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting CommandsService");
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "CommandsService terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
