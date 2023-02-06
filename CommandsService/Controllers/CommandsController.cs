f3da50741198229d5935da921a93518bd4853521
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-06 17:21:17 +0100

    Add missing PlatformId

--- CommandsService/Controllers/CommandsController.cs
+++ CommandsService/Controllers/CommandsController.cs
@@ -63,6 +63,8 @@ public class CommandsController : ControllerBase
             return NotFound();
         }
 
+        command.PlatformId = platformId;
+
         var commandEntity = _mapper.Map<Command>(command);
         
         _commandsRepository.Create(commandEntity);
